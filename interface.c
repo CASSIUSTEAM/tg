3d167bf0e7f4c0348cb77d7762ef8fd47ba64507
ArashEmpix <ArashEmpix@users.noreply.github.com>
2016-06-19 01:01:22 -0700

    Update interface.c

--- interface.c
+++ interface.c
@@ -845,6 +845,7 @@ void do_msg (struct command *command, int arg_num, struct arg args[], struct in_
   assert (arg_num == 2);
   if (ev) { ev->refcnt ++; }
   vlogprintf (E_DEBUG, "reply_id=%d, disable=%d\n", reply_id, disable_msg_preview);
+  do_html = TGLMF_HTML;
   tgl_do_send_message (TLS, args[0].peer_id, ARG2STR(1), TGL_SEND_MSG_FLAG_REPLY(reply_id) | disable_msg_preview | do_html, NULL, print_msg_success_gw, ev);
 }
 
